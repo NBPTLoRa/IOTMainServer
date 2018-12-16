aa720d5f0de5c8a28c036c99fa305742c792b888
Xy718 <Administrator@jzf>
2018-12-16 16:50:42 +0800

    加了个Jar包，而不是更改，反正每次报错都加就完事了

--- src/web/mainservlet/LoginEntrance.java
+++ src/web/mainservlet/LoginEntrance.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
 /**
  * Servlet implementation class LoginEntrance
