257801e0f383cb8f652d7fd0f34c86e90b09934b
Xy718 <869839000@qq.com>
2018-12-16 13:25:06 +0800

    login的业务代码加了一些

--- src/web/mainservlet/LoginEntrance.java
+++ src/web/mainservlet/LoginEntrance.java
@@ -1,11 +1,17 @@
 package web.mainservlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 /**
  * Servlet implementation class LoginEntrance
@@ -25,8 +31,34 @@ public class LoginEntrance extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		String userID;
+		String pwd;
+		JsonObject retJ=new JsonObject();
+		try
+		{
+			userID=request.getParameter("userID");
+			pwd=request.getParameter("pwd");
+			Boolean loginFlag=false;
+			//校对数据库↓
+			
+			if(loginFlag)
+			{
+				//校对成功返回{"login":"success","error":"0"}
+				retJ=new JsonParser().parse("{\"login\":\"success\",\"error\":\"0\"}").getAsJsonObject();
+			}else
+			{
+				//校对失败返回{"login":"failed","error":"用户名或密码不正确"}
+				retJ=new JsonParser().parse("{\"login\":\"failed\",\"error\":\"用户名或密码不正确\"}").getAsJsonObject();
+			}
+		}
+		catch (Exception e) {
+			//报错：
+			//返回：{"login":"error","error":"e.toString()"}
+			retJ=new JsonParser().parse("{\"login\":\"error\",\"error\":\""+e.toString()+"\"}").getAsJsonObject();
+		}finally {
+			out.println(retJ);
+		}
 	}
 
 	/**
