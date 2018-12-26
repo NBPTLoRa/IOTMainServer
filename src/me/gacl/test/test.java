bf17684928946a9109314e8a870cb4d34849a129
sun5724 <2404960138@qq.com>
2018-12-26 19:57:21 +0800

    修改了数据库的连接代码

--- src/me/gacl/test/test.java
+++ src/me/gacl/test/test.java
@@ -6,11 +6,7 @@ public class test {
 
     public static void main(String[] args){
     	SqlOp sql=new SqlOp();
-    	String[]a=sql.getDisServIP();
-    	System.out.println(a[0]);
-    	
-    	String[]b=sql.getDisServIP();
-    	System.out.println(b[0]);
+    	System.out.println(sql.getProfNameforappName("1"));
     }
 
 }
