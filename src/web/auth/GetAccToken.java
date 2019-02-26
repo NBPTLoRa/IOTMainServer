719449e611995baa87b5ed6db76ef854de9a9756
Xy718 <Administrator@jzf>
2019-02-26 21:58:58 +0800

    jzf：获取临时授权快做完了

--- src/web/auth/GetAccToken.java
+++ src/web/auth/GetAccToken.java
@@ -1,11 +1,17 @@
 package web.auth;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import web.sqloperation.SqlOp;
 
@@ -30,16 +36,90 @@ public class GetAccToken extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out=response.getWriter();
 		String userID=request.getParameter("userID");
 		String ApiKey=request.getParameter("openAPIKey");
+		String Client_ID=request.getParameter("client_id");
+		
+		
+		String retToken="0";
+		String retTime="0";
+		String retError="e:CreateNullError";
+		
+		JsonObject retJ =new JsonObject();
+		JsonParser jsonParser=new JsonParser();
 		
 		//查找有没有符合的用户
+		String retS=sqlOp.getAPI(userID);
 		
-		//查看用户的ApiKey的时间，如果超过6小时
-		
-		//就刷新accessToken
+		if(retS.equals("0"))
+		{
+			retToken="0";
+			retTime="0";
+			retError+="Your account does not enable third-party development!";
+		}else if(retS.substring(0, 1).equals("e"))
+		{
+			retToken="0";
+			retTime="0";
+			retError+="Sql Get errors-"+retS;
+		}
+		else
+		{//库内APIKey获取正常
+			if(retS.split(":")[1].equals(ApiKey))
+			{//API符合
+				//查看clien_id是否重复
+				String tempS="";//sqlOp.hasClient_id(Client_ID,ApiKey);
+				if(tempS.equals("1"))
+				{//有正在使用的client_ID
+
+					retToken="0";
+					retTime="0";
+					retError+="Client_id is in use!";
+				}
+				else if(tempS.substring(0, 1).equals("e"))
+				{//出错
+					retToken="0";
+					retTime="0";
+					retError+="Sql Get errors-"+retS;
+				}
+				else
+				{//没有重复且在使用的client_ID
+					//在authToken库创建一条APIkey
+					String accessToken=randomHexString(64);
+					String r="e:CreateR";
+					//r=sqlOp.createTempAuth(ApiKey,Client_ID,accessToken,6);
+					
+					if(r.equals("1"))
+					{//设置正常
+						retToken=accessToken;
+						retTime="0";
+						retError+="";
+					}
+					else
+					{//设置失败
+
+						retToken="0";
+						retTime="0";
+						retError+="Sql Get errors-"+retS;
+					}
+				}
+				
+			}else
+			{
+				retToken="0";
+				retTime="0";
+				retError+="APIKey Error!";
+			}
+			
+		}
 		
 		//返回accessToken
+		retJ=jsonParser.parse("{\"accessToken\":\""+retToken
+				+"\",\"error\":\""+retError.replace("\"","#").replace("e:CreateNullError", "")//
+				+"\",\"ExpirationTime\":\""+retTime
+				+"\"}").getAsJsonObject();
+		out.println(retJ);
 	}
 
 	/**
@@ -49,5 +129,19 @@ public class GetAccToken extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
+	public static String randomHexString(int len)  {
+		try {
+			StringBuffer result = new StringBuffer();
+			for(int i=0;i<len;i++) {
+				result.append(Integer.toHexString(new Random().nextInt(16)));
+			}
+			return result.toString().toUpperCase();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			
+		}
+		return null;
+	}
 }
