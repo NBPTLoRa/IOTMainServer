424863b9bdbc45202b1f90079f1689c848554ef4
Xy718 <jzf869839000@gmail.com>
2019-03-26 21:59:35 +0800

    jzf:data部分

--- src/lora/mainservlet/datashow/TotalCount.java
+++ src/lora/mainservlet/datashow/TotalCount.java
@@ -1,33 +1,82 @@
 package lora.mainservlet.datashow;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import lora.auth.Auth;
+import lora.mainservlet.DeviceADD;
+import lora.mainservlet.UrlApi;
+import web.loginVerify.LoginObj;
+import web.sqloperation.SqlOp;
+
 /**
  * Servlet implementation class TotalCount
  */
-@WebServlet("/TotalCount")
 public class TotalCount extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
+	 /**
+     * @see HttpServlet#HttpServlet()
+     */
+	SqlOp sqlOp;
     public TotalCount() {
         super();
         // TODO Auto-generated constructor stub
+        sqlOp=new SqlOp();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// TODO Auto-generated method stubresponse.setContentType("application/json;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+
+		LoginObj loginObj=Auth.tokenLogin(request);
+		
+		String TotalDevice="";
+		String NetWorkedDevice="";
+		String TotalData="";
+		String DayData="";
+		String retError="CreateNull";
+		
+		
+		JsonObject retJ=new JsonObject();
+		JsonParser jsonParser=new JsonParser();
+		
+		//≈–∂œ”√ªßº¯»®
+		Boolean authFlag=false;
+		if(loginObj.getLoginSta())
+		{
+			authFlag=true;
+		}else
+		{
+			retError+="Your account does not have permission!"+loginObj.getException();
+		}
+		
+		
+		String retJsonS="{"
+				+"\"TotalDevice\":\""+TotalDevice+"\","
+				+"\"NetWorkedDevice\":\""+NetWorkedDevice+"\","
+				+"\"TotalData\":\""+TotalData+"\","
+				+"\"DayData\":\""+DayData+"\","
+				+"\",\"error\":\""+retError.replace("\"","#").replace("CreateNull", "")
+				+"\"}";
+		retJ=jsonParser.parse(retJsonS).getAsJsonObject();
+		out.println(retJ);
 	}
 
 	/**
