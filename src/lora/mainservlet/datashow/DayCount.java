0e9d1fc19806e89d5f480e0613bf17eb0d8d7453
Xy718 <869839000@qq.com>
2019-04-12 17:35:13 +0800

    jzf:day一半

--- src/lora/mainservlet/datashow/DayCount.java
+++ src/lora/mainservlet/datashow/DayCount.java
@@ -1,12 +1,19 @@
 package lora.mainservlet.datashow;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import lora.auth.Auth;
+import web.loginVerify.LoginObj;
 import web.sqloperation.SqlOp;
 
 /**
@@ -30,7 +37,50 @@ public class DayCount extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out=response.getWriter();
 		
+		LoginObj loginObj=Auth.tokenLogin(request);
+		
+		String Smoke="";
+		String Temperature="";
+		String Humidity="";
+		String Parklot="";
+		String Safety="";
+		String retError="CreateNull";
+		
+		JsonObject retJ=new JsonObject();
+		JsonParser jsonParser=new JsonParser();
+		
+		//≈–∂œ”√ªßº¯»®
+		Boolean authFlag=false;
+		if(loginObj.getLoginSta())
+		{
+			authFlag=true;
+		}else
+		{
+			retError+="Your account does not have permission!"+loginObj.getException();
+		}
+		
+		if(authFlag)
+		{		
+			Smoke="30.30";
+			Temperature="20.00";
+			Humidity="10.30";
+			Parklot="30.00";
+			Safety="9.40";
+		}
+		
+		String retJsonS="{"
+				+"\"Smoke\":\""+Smoke+"\","
+				+"\"Temperature\":\""+Temperature+"\","
+				+"\"Humidity\":\""+Humidity+"\","
+				+"\"Parklot\":\""+Parklot+"\","
+				+"\"Safety\":\""+Safety+"\","
+				+"\"error\":\""+retError.replace("\"","#").replace("CreateNull", "")
+				+"\"}";
+		retJ=jsonParser.parse(retJsonS).getAsJsonObject();
+		out.println(retJ);
 	}
 
 	/**
