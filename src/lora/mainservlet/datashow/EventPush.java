1ab424e0f863306c0e10e4c0df665ebf0756cc53
Xy718 <jzf869839000@gmail.com>
2019-04-16 14:24:42 +0800

    jzf:建了eventPush

---
+++ src/lora/mainservlet/datashow/EventPush.java
@@ -0,0 +1,101 @@
+package lora.mainservlet.datashow;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import lora.auth.Auth;
+import web.loginVerify.LoginObj;
+import web.sqloperation.SqlOp;
+
+/**
+ * Servlet implementation class EventPush
+ */
+
+public class EventPush extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	SqlOp sqlOp;
+    public EventPush() {
+        super();
+        sqlOp=new SqlOp();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out=response.getWriter();
+		
+		LoginObj loginObj=Auth.tokenLogin(request);
+		
+		String TotalDevice="";
+		String NetWorkedDevice="";
+		String TotalData="";
+		String DayData="";
+		String retError="CreateNull";
+		
+		JsonObject retJ=new JsonObject();
+		JsonParser jsonParser=new JsonParser();
+		
+		//≈–∂œ”√ªßº¯»®
+		Boolean authFlag=false;
+		if(loginObj.getLoginSta())
+		{
+			authFlag=true;
+		}else
+		{
+			retError+="Your account does not have permission!"+loginObj.getException();
+		}
+		
+		if(authFlag)
+		{
+			String total=sqlOp.getTotalCount();
+			if(total.split(":")[0].equals("1"))
+			{
+				String s=total.substring(2);
+				TotalDevice=	s.split(",")[0];
+				NetWorkedDevice=s.split(",")[1];
+				TotalData=		s.split(",")[2];
+				DayData=		s.split(",")[3];
+			}
+			else
+			{
+				retError=total.split(":")[1];
+			}
+		}
+		
+		String retJsonS="{"
+				+"\"TotalDevice\":\""+TotalDevice+"\","
+				+"\"NetWorkedDevice\":\""+NetWorkedDevice+"\","
+				+"\"TotalData\":\""+TotalData+"\","
+				+"\"DayData\":\""+DayData+"\","
+				+"\"error\":\""+retError.replace("\"","#").replace("CreateNull", "")
+				+"\"}";
+		retJ=jsonParser.parse(retJsonS).getAsJsonObject();
+		out.println(retJ);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
