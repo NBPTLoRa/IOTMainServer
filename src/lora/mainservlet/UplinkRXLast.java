9bad7895ef19a2f69e0cbb0c95972b795aa493c0
Xy718 <jzf869839000@gmail.com>
2019-04-20 15:40:09 +0800

    1

--- src/lora/mainservlet/UplinkRXLast.java
+++ src/lora/mainservlet/UplinkRXLast.java
@@ -2,12 +2,10 @@ package lora.mainservlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
