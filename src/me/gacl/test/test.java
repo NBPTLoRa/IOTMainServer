41c5a4aaa83a268670543769be6783d4b9aec302
sun5724 <2404960138@qq.com>
2019-05-14 20:48:58 +0800

    SYT

--- src/me/gacl/test/test.java
+++ src/me/gacl/test/test.java
@@ -6,6 +6,6 @@ public class test {
 
     public static void main(String[] args){
     	SqlOp sql=new SqlOp();
-    	System.out.println(sql.getSwitches());
+    	System.out.println(sql.getSwitches());//ddd
     }
 }
