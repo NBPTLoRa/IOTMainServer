e74c756bdff679cb01a2c78592f5baddea994b56
Xy718 <jzf869839000@gmail.com>
2019-05-14 20:39:42 +0800

    jzf:1

--- src/lora/switches/getSwitches.java
+++ src/lora/switches/getSwitches.java
@@ -1,32 +1,63 @@
-package lora.switchs;
+package lora.switches;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import lora.auth.Auth;
+import web.loginVerify.LoginObj;
+import web.sqloperation.SqlOp;
+
 /**
- * Servlet implementation class getLight
+ * Servlet implementation class getSwitches
  */
-public class getLight extends HttpServlet {
+@WebServlet("/getSwitches")
+public class getSwitches extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public getLight() {
+	SqlOp sqlOp;
+    public getSwitches() {
         super();
+        sqlOp=new SqlOp();
         // TODO Auto-generated constructor stub
     }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+
+		//鉴权
+		LoginObj loginObj=Auth.tokenLogin(request);
+
+		//返回的json
+		JsonObject retJ=new JsonObject();
+		JsonParser jsonParser=new JsonParser();
+		
+		String retDevCount="0";
+		String retDevList="";
+		String retError="CreateNull";	//返回的错误信息
+		
+		
+		
+		retJ=jsonParser.parse("{\"DevCount\":\""+retDevCount
+				+retDevList
+				+"\"error\":\""+retError.replace("\"","#").replace("CreateNull", "")//
+				+"\"}").getAsJsonObject();
+		out.println(retJ);
 	}
 
 	/**
---
+++ src/lora/switches/setLight.java
@@ -0,0 +1,72 @@
+package lora.switches;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import lora.auth.Auth;
+import web.loginVerify.LoginObj;
+import web.sqloperation.SqlOp;
+
+/**
+ * Servlet implementation class setLight
+ */
+@WebServlet("/setLight")
+public class setLight extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	SqlOp sqlOp;
+    public setLight() {
+        super();
+        sqlOp=new SqlOp();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+
+		//鉴权
+		LoginObj loginObj=Auth.tokenLogin(request);
+
+		//返回的json
+		JsonObject retJ=new JsonObject();
+		JsonParser jsonParser=new JsonParser();
+		
+		String retDevCount="0";
+		String retDevList="";
+		String retError="CreateNull";	//返回的错误信息
+		
+		
+		
+		retJ=jsonParser.parse("{\"DevCount\":\""+retDevCount
+				+retDevList
+				+"\"error\":\""+retError.replace("\"","#").replace("CreateNull", "")//
+				+"\"}").getAsJsonObject();
+		out.println(retJ);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
